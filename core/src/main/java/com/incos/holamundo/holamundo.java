87f2e99c6ea54087932cea00fca33d99cc697523
jorge <jf61530622@gmail.com>
2025-11-29 12:10:30 -0400

    practica 1

--- core/src/main/java/com/incos/holamundo/holamundo.java
+++ core/src/main/java/com/incos/holamundo/holamundo.java
@@ -2,31 +2,36 @@ package com.incos.holamundo;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class holamundo extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    private SpriteBatch lapiz;
+    private Texture miImagen;
+    private BitmapFont miTexto;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        lapiz = new SpriteBatch();
+        miImagen = new Texture("ferrari_logo.png");//para un nuevo texto
+        miTexto = new BitmapFont();
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        ScreenUtils.clear(0, 0, 0, 1);
+        lapiz.begin();
+        lapiz.draw(miImagen, 140, 210);
+        miTexto.draw(lapiz, "Hola, este es mi primer juego!", 50, 200);
+        lapiz.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        lapiz.dispose();
+        miImagen.dispose();
+        miTexto. dispose();
     }
 }
