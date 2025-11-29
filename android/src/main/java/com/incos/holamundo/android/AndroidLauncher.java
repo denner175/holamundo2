3ce6128303abe7e3136849a6a980fdd4b96d2535
jorge <jf61530622@gmail.com>
2025-11-29 11:22:56 -0400

    Initial commit

---
+++ android/src/main/java/com/incos/holamundo/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.incos.holamundo.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.incos.holamundo.holamundo;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new holamundo(), configuration);
+    }
+}
